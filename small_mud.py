2606d67cc8524c2ebd6be0e9bbc22a314d5391a1
Felix Hansson <85492626+SHOOPIDOOP@users.noreply.github.com>
2024-01-16 10:23:04 +0100

    Add files via upload

--- small_mud.py
+++ small_mud.py
@@ -1,7 +1,7 @@
 
 import socket
 
-host = "127.0.0.1"
+host = "192.168.1.111"
 port = 65432
 
 """
@@ -9,18 +9,18 @@ small_mud.py, Thomas Lundqvist, 2019-2023, use freely!
 
 A small beginning of a MUD, Multi User Dungeon, game.
 """
-
+s = socket.socket()
+s.connect((host, port)) 
 finished = 0
 print("Welcome to simple MUD, the simple Multi-User Dungeon game.")
 while not finished:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((host, port))     
-        command = input(': ')
-        if command == "quit" or command == "q":
-            finished = 1
-        else:
-            s.sendall(bytes(command, 'utf-8'))
-            data = s.recv(1024)
+       
+    command = input(': ')
+    if command == "quit" or command == "q":
+        finished = 1
+    else:
+        s.send(bytes(command, 'utf-8'))
+        data = s.recv(1024)
 
     print(f"Recieved {data!r}")
 
